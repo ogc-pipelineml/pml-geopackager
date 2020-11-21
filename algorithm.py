2c0c3706879f3cf024e4ca0d2eecc8ebf845d320
Jeff Bourdier <jeffbourdier@gmail.com>
2020-11-21 14:27:55 -0600

    Create (not populate) GeoPackage layers

--- algorithm.py
+++ algorithm.py
@@ -19,22 +19,43 @@
 This module defines the 'PipelineMLGeoPackagerAlgorithm' class.
 """
 
-# Import (from the standard 'os' module) the 'path' sub-module.
 from os import path
-
-# Import (from the 'osgeo' package) the 'gdal' sub-module.
+from threading import Event
+from xml.parsers import expat
 from osgeo import gdal
-
-# Import (from the 'core' module in the 'qgis' package) the
-# 'QgsProcessingAlgorithm', 'QgsProcessingParameterFile',
-# and 'QgsProcessingParameterFileDestination' classes.
+from osgeo import ogr
 from qgis.core import (
-  QgsProcessingAlgorithm, QgsProcessingParameterFile,
-  QgsProcessingParameterFileDestination)
-
-# Import (from the 'QtGui' module in the
-# 'qgis.PyQt' package) the 'QIcon' class.
+  QgsProcessingAlgorithm, QgsProcessingContext, QgsProcessingFeedback,
+  QgsProcessingParameterFile, QgsProcessingParameterFileDestination)
 from qgis.PyQt.QtGui import QIcon
+
+# This list serves as a stack of XML elements.
+_stack = []
+
+# This event is used to signal completion of the XML parsing thread.
+_event = Event()
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+# Define the 'handle_element_start' function, which is called (by the
+# XML parser) for the start of every element in the PipelineML file.
+def handle_element_start(name: str, attributes: dict) -> None:
+    global _feedback
+    global _dataset
+    if len(_stack) > 0 and _stack[-1] == 'component':
+        _feedback.pushInfo('Creating ' + name + ' layer')
+        _dataset.CreateLayer(name, None, ogr.wkbUnknown)
+    _stack.append(name)
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+# Define the 'handle_element_end' function, which is called (by the
+# XML parser) for the end of every element in the PipelineML file.
+def handle_element_end(name: str) -> None:
+    _stack.pop()
+
+    # If the stack is empty, XML parsing
+    # is complete.  Signal the main thread.
+    if len(_stack) < 1:
+        _event.set()
 
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 # Define the 'PipelineMLGeoPackagerAlgorithm' class
@@ -49,7 +70,7 @@ class PipelineMLGeoPackagerAlgorithm(QgsProcessingAlgorithm):
     # Override the 'groupId' method, which should return a fixed,
     # non-localized string containing only lowercase alphanumeric
     # characters and no spaces or other formatting characters.
-    def groupId(self):
+    def groupId(self) -> str:
         """
         This method returns a string that uniquely
         identifies the group within a provider.
@@ -59,7 +80,7 @@ class PipelineMLGeoPackagerAlgorithm(QgsProcessingAlgorithm):
     # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
     # Override the 'group' method, which
     # should return a localized string.
-    def group(self):
+    def group(self) -> str:
         """
         This method returns the name of the
         group to which this algorithm belongs.
@@ -70,7 +91,7 @@ class PipelineMLGeoPackagerAlgorithm(QgsProcessingAlgorithm):
     # Override the 'name' method, which should return a fixed,
     # non-localized string containing only lowercase alphanumeric
     # characters and no spaces or other formatting characters.
-    def name(self):
+    def name(self) -> str:
         """
         This method returns a string that uniquely
         identifies the algorithm within a provider.
@@ -80,7 +101,7 @@ class PipelineMLGeoPackagerAlgorithm(QgsProcessingAlgorithm):
     # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
     # Override the 'displayName' method, which should return a
     # localized string of no more than 4 words, using sentence case.
-    def displayName(self):
+    def displayName(self) -> str:
         """
         This method returns a user-friendly algorithm name.
         """
@@ -88,16 +109,18 @@ class PipelineMLGeoPackagerAlgorithm(QgsProcessingAlgorithm):
 
     # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
     # Override the 'icon' method.
-    def icon(self):
+    def icon(self) -> QIcon:
         """
         This method returns an icon for the algorithm.
         """
-        return QIcon(path.join(path.dirname(__file__), 'icon.svg'))
+        name = path.dirname(__file__)
+        name = path.join(name, 'icon.svg')
+        return QIcon(name)
 
     # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
     # Override the 'createInstance' method, which should
     # return a pristine instance of the algorithm class.
-    def createInstance(self):
+    def createInstance(self) -> 'PipelineMLGeoPackagerAlgorithm':
         """
         This method creates a new instance of the algorithm class.
         """
@@ -106,7 +129,7 @@ class PipelineMLGeoPackagerAlgorithm(QgsProcessingAlgorithm):
     # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
     # Override the 'initAlgorithm' method, which should add
     # all required input parameters and output definitions.
-    def initAlgorithm(self, config):
+    def initAlgorithm(self, config: 'Dict[str, Any]' = {}) -> None:
         """
         This method initializes the algorithm
         using the specified configuration.
@@ -126,27 +149,49 @@ class PipelineMLGeoPackagerAlgorithm(QgsProcessingAlgorithm):
     # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
     # Override the 'processAlgorithm' method, which should implement
     # processing logic and return a map of algorithm outputs.
-    def processAlgorithm(self, parameters, context, feedback):
+    def processAlgorithm(self, parameters: 'Dict[str, Any]',
+            context: QgsProcessingContext,
+            feedback: QgsProcessingFeedback) -> 'Dict[str, Any]':
         """
         This method runs the algorithm using the specified parameters.
         """
 
         # Retrieve the value of the output parameter
         # (i.e., the GeoPackage file destination).
-        filePath = self.parameterAsFileOutput(parameters, 'OUTPUT', context)
+        gpkg_path = self.parameterAsFileOutput(parameters, 'OUTPUT', context)
 
         # Fetch the GDAL driver for the GeoPackage file format.
         driver = gdal.GetDriverByName('GPKG')
 
-        # Use the driver to create a new dataset (i.e., a new
-        # GeoPackage file).  Every argument except the first is
-        # related to raster dimensions and does not apply to
-        # vector-only datasets (such as the one we're creating).
-        dataset = driver.Create(filePath, 0, 0, 0, gdal.GDT_Unknown)
+        # Use the driver to create a new
+        # dataset (i.e., a new GeoPackage).
+        global _dataset
+        _dataset = driver.Create(gpkg_path, 0, 0, 0, gdal.GDT_Unknown)
+
+        # Retrieve the value of the input
+        # parameter (i.e., the PipelineML file).
+        pml_path = self.parameterAsFile(parameters, 'INPUT', context)
+
+        # Open the PipelineML file for reading.  (Note that the XML
+        # parser requires the file to be opened in binary mode.)
+        with open(pml_path, 'rb') as pml_file:
+            global _feedback
+            _feedback = feedback
+
+            # Parse the PipelineML file.  (The handler functions
+            # called by the XML parser populate the GeoPackage
+            # based on the PipelineML file's contents.)
+            parser = expat.ParserCreate()
+            parser.StartElementHandler = handle_element_start
+            parser.EndElementHandler = handle_element_end
+            parser.ParseFile(pml_file)
+
+        # Wait for the XML parsing thread to signal completion.
+        _event.wait()
 
         # Close the dataset to ensure that all data is written
         # and resources are recovered (file handle closed, etc.).
-        dataset = None
+        _dataset = None
 
         # Return the output.
-        return {'OUTPUT': filePath}
+        return {'OUTPUT': gpkg_path}
